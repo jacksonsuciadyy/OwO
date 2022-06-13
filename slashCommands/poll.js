110fcff97f00e9fa5e3931945d410496986615ea
jacksonsuciady <jacksonsuciady@gmail.com>
2022-06-13 23:44:44 +0700

    fix question validation

--- slashCommands/poll.js
+++ slashCommands/poll.js
@@ -26,7 +26,7 @@ const run = async(client, interaction) => {
     let choice_r = interaction.options.getString("choice_r")
     console.log("Finished declare question & choices") 
 
-    if(question != null) return interaction.reply("Invalid question!")
+    if(question == null) return interaction.reply("Invalid question!")
 
     let questionMsg = `📊 ${question}`;
     let answerMsg = "";
