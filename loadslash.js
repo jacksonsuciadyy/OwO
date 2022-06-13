a31ca546a41ea2f81b8bc02cba3d9bb714be87c8
jacksonsuciady <jacksonsuciady@gmail.com>
2022-06-13 23:53:28 +0700

    modify loadslash

--- loadslash.js
+++ loadslash.js
@@ -18,16 +18,19 @@ let bot = {
 client.loadSlashCommands = (bot, reload) => require("./handlers/slashCommandsHandler")(bot, reload)
 client.loadSlashCommands(bot, false)
 
-const guildID = "874493088024899595"
+// const guildID = "874493088024899595"
 
 client.on("ready", async () => {
     console.log(`Loading ${client.slashCommands.size} slash commands`)
-
-    const guild = client.guilds.cache.get(guildID)
+    const Guilds = client.guilds.cache.map(guild => guild.id);
+    Guilds.forEach((guildID) => {
+        const guild = client.guilds.cache.get(guildID)
     if (!guild)
         console.error("Target Guild not found")
 
     await guild.commands.set([...client.slashCommands.values()])
+    });
+    
     console.log("Finished")
     process.exit(0)
 })
